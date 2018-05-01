629d7fdedbff5166ed3ad7f57a646ce318935457
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-05-01 10:48:56 -0400

    added place holders to keep focus

--- lib/top_six/album.rb
+++ lib/top_six/album.rb
@@ -1,7 +1,15 @@
 class TopSix::Album
+  attr_accessor :title, :artist, :genre, :rating, :review
 
+    def self.best_new
+      # gather scraped album information from Pitchfork list and returns the data needed.
+      #Artist, Tittle, and Genre
 
+    end
 
+    def self.review
+      #returns the review snippet & rating of the corresponding album from second scrape
+    end
 
 
 
