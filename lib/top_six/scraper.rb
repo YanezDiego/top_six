a6566bf6fcfeda0648a10eade671c225c8b30b36
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-05-01 13:06:28 -0400

    scraped album page

--- lib/top_six/scraper.rb
+++ lib/top_six/scraper.rb
@@ -1,4 +1,25 @@
 class TopSix::Scraper
 
+    def self.scrape_top_album_page
+      doc = Nokogiri::HTML(open("https://pitchfork.com/best"))
+      binding.pry
+      albums = []
+
+      doc.css("section#best-new-albums").each do |album|
+        album_title = album.css("h2.title").text
+        album_artist = album.css("ul2").text
+        album_genre = album.css('a.genre-list__link').text
+        albums << {title: album_title, artist: album_artist, genre: album_genre}
+      end
+      albums
+    end
+    #doc.css("section#best-new-albums").css("ul.artist-list").text #<=artist list
+    #doc.css("section#best-new-albums").css("h2.title").text #<= album title
+    #doc.css("section#best-new-albums").css('a.genre-list__link').text #<- album genre
+
+
 
 end
+
+
+TopSix::Scraper.scrape_top_album_page
