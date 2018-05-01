ed6f4dd1f1451711924a942e172db7eeb2f85fef
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-05-01 13:50:37 -0400

    fixed an error for artist

--- lib/top_six/scraper.rb
+++ lib/top_six/scraper.rb
@@ -2,13 +2,13 @@ class TopSix::Scraper
 
     def self.scrape_top_album_page
       doc = Nokogiri::HTML(open("https://pitchfork.com/best"))
-      binding.pry
       albums = []
 
       doc.css("section#best-new-albums").each do |album|
         album_title = album.css("h2.title").text
-        album_artist = album.css("ul2").text
+        album_artist = album.css("ul.artist-list").text
         album_genre = album.css('a.genre-list__link').text
+        binding.pry
         albums << {title: album_title, artist: album_artist, genre: album_genre}
       end
       albums
