f099bed91785992c38f240cead4ea58e42977d36
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-05-01 13:08:18 -0400

    added relative scraper

--- lib/top_six.rb
+++ lib/top_six.rb
@@ -1,2 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "./top_six/version"
+require_relative "./top_six/album"
 require_relative "./top_six/cli"
+require_relative "./top_six/scraper"
