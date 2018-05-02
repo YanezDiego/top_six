43b8c8f5349502f7c9f87b44275cf5ab58f9d551
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-05-02 17:40:33 -0400

    fixed enviroment dependency

--- lib/top_six.rb
+++ lib/top_six.rb
@@ -5,4 +5,5 @@ require 'pry'
 require_relative "./top_six/version"
 require_relative "./top_six/album"
 require_relative "./top_six/cli"
+require_relative "./top_six/review"
 require_relative "./top_six/scraper"
