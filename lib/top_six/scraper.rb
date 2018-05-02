a74de287d0a8d5da26dc3122cd7ea197ecc922a2
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-05-02 17:37:24 -0400

    instantiating new album instances on topalbum sraper

--- lib/top_six/scraper.rb
+++ lib/top_six/scraper.rb
@@ -1,38 +1,36 @@
 class TopSix::Scraper
 
+    def initialize
+
+    end
+
+
     def self.scrape_top_album_page
       doc = Nokogiri::HTML(open("https://pitchfork.com/best"))
 
-      albums = []
-
       doc.css('section#best-new-albums div.bnm-small.album-small').each do |album|
-        album_review_link = album.css("a.link-block").attribute("href").value
-        album_title = album.css("h2.title").text
-        album_artist = album.css('ul.artist-list li').text
-        album_genre = album.css('a.genre-list__link').text
-        albums << {title: album_title, artist: album_artist, genre: album_genre, url:album_review_link}
+        a = TopSix::Album.new
+        a.link = album.css("a.link-block").attribute("href").value
+        a.title = album.css("h2.title").text
+        a.artist = album.css('ul.artist-list li').text
+        a.genre = album.css('a.genre-list__link').text
+        a.save
       end
-      binding.pry
-      albums
+
+      TopSix::Album.all
+    end
+
+    def self.get_review(input)
+      doc = Nokogiri::HTML(open("http://www.pitchfork.com#{TopSix::Album.all[input - 1].link}")) #scrapes the review HTML
     end
 
     def self.scrape_review
-     #when doc = Nokogiri::HTML(open()) scrapes the review HTML
+      doc
     end
 
 end
 
-
+#TopSix::Scraper.scrape_review
 #TopSix::Scraper.scrape_top_album_page
 
-
-#doc.css('div.bnm-small.album-small').css('ul.artist-list li')
-#.css('h2.title')
-#.css('a.genre-list__link')
-
-
-#doc.css("section#best-new-albums").each do |album|
-#  album_title = album.css("h2.title").collect{|title| title.text}
-#  album_artist = album.css("ul.artist-list").collect{|artist| artist.text}
-#  album_genre = album.css('a.genre-list__link').collect{|genre| genre.text}
-#  albums << {title: album_title, artist: album_artist, genre: album_genre}
+#doc = Nokogiri::HTML(open("http://www.pitchfork.com#{TopSix::Album.all[input - 1].link}"))
