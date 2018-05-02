0ec087c5d1e167517f336942c80772f2a345d82e
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-05-02 17:38:41 -0400

    added real data to #top_albums

--- lib/top_six/cli.rb
+++ lib/top_six/cli.rb
@@ -5,14 +5,17 @@ class TopSix::CLI
   def call
     puts "---Welcome to top 6---!"
     top_albums
-    menu
-    goodbye
+    # menu
+    # goodbye
   end
 
   def top_albums
     puts "------TOP ALBUMS-----------"
-    @albums = TopSix::Album
-    puts "1. Teen Spirit - Nirvana - Grunge\n2. Care For Me - Saba - rap"
+    TopSix::Scraper.scrape_top_album_page.each_with_index do |album, i|
+      puts "#{i + 1}. #{album.artist} - #{album.title} - #{album.genre} - #{album.link}"#<- list the albums with artist and genre that are reviewed
+    end
+
+    menu
   end
 
   def menu
@@ -22,7 +25,7 @@ class TopSix::CLI
     while input != "exit"
       puts "-----------------"
       puts "Please enter 1-6 to see more details.\nType'list' to see the list again.\nType 'exit' to exit"
-    input = gets.strip.downcase
+      input = gets.strip.downcase
       case input
       when "1"
         puts 'Nirvana abstrackt review'
