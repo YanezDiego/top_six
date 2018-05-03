7dbbe19881a4302f1f2e532694693bc85c683601
Diego Yanez <diegoyanez@MacBook-Pro-2.fios-router.home>
2018-05-03 17:45:18 -0400

    instatiates review from scraper

--- lib/top_six/review.rb
+++ lib/top_six/review.rb
@@ -1,6 +1,18 @@
 class TopSix::Review
+  attr_accessor :rating, :review
 
+@@reviews = []
 
+    def initialize
 
+    end
+
+    def self.all
+      @@reviews
+    end
+
+    def save
+      @@reviews << self
+    end
 
 end
