9dc5b8aafe10228d5d781f16fdd94b461e1a884d
howardbdev <howarddevennish@gmail.com>
2018-05-14 09:47:35 -0400

    add notes

--- lib/top_six/cli.rb
+++ lib/top_six/cli.rb
@@ -25,6 +25,9 @@ class TopSix::CLI
   def top_albums
     system('clear')
     puts "   Top Albums"
+    # is there a way that we could check and see if we've already scraped for the top albums?
+    # if we have, then we could just grab them from Album.all rather than re-scraping
+    # We could build a finder method in the album class such as .find_by_title or .find_by_artist
     TopSix::Scraper.scrape_top_album_page.each_with_index do |album, i|
       puts "#{i + 1}. #{album.artist} - #{album.title.gsub("â", "")} - #{album.genre}"#<- list the albums with artist and genre that are reviewed
     end
@@ -36,6 +39,10 @@ class TopSix::CLI
     puts "Please choose from 1 to 6 to select which album you would like to see the review from."
     input = gets.strip.to_i
     if input <= 6
+      # same idea as the suggestion above:  what if we've already scraped For
+      # a particular review?  let's check and see if it exists before scraping again
+      # In order to accomplish this, we could build check to see if the album's review has been populated
+      # Also, if this is just for a single Album, is there a reason we're iterating here?
     TopSix::Scraper.get_review(input - 1).each do |album|
       puts "#{album.review}"
       puts "Score: #{album.rating}"
