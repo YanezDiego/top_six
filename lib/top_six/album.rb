144246e6d4efce0350e078d4edb786115cfa8879
howardbdev <howarddevennish@gmail.com>
2018-05-14 09:50:25 -0400

    one more note

--- lib/top_six/album.rb
+++ lib/top_six/album.rb
@@ -19,6 +19,13 @@ class TopSix::Album
       @@albums.clear
     end
 
+    def self.find_by_title(title)
+      # how could we build out this method to help us find an Album?
+      # Or, suppose we wanted to allow different albums to have the same title,
+      # in that case, we would have to check artist and title...
+      # how we make that happen?
+    end
+
     def review
       #returns the review snippet & rating of the corresponding album from second page
       TopSix::Review.all.each do |i|
